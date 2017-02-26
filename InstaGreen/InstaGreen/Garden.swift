05aaff015bcca01b3ae24fb1f6da696c8588b24f
Mira Estil <imz.estil@gmail.com>
2017-02-26 10:11:10 -0500

    merging

---
+++ InstaGreen/InstaGreen/Garden.swift
@@ -0,0 +1,24 @@
+//
+//  Garden.swift
+//  InstaGreen
+//
+//  Created by Ilmira Estil on 2/26/17.
+//  Copyright © 2017 C4Q. All rights reserved.
+//
+
+import Foundation
+
+class Garden {
+    let address: String
+    let boro: String
+    let name: String
+    let neighborhood: String
+    
+    init(address: String, boro: String, name: String, neighborhood: String) {
+        self.address = address
+        self.boro = boro
+        self.neighborhood = neighborhood
+    }
+    
+    //convenience init?(from dict: [String: String])
+}
