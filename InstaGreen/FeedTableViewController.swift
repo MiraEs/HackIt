4026dcd37a225ce6041f3795ec920e72436ce866
Mira Estil <imz.estil@gmail.com>
2017-02-26 13:20:31 -0500

    updates on feed TBV

--- InstaGreen/FeedTableViewController.swift
+++ InstaGreen/FeedTableViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import Firebase
 
 class FeedTableViewController: UITableViewController {
-    
+    var databaseRef: FIRDatabaseReference!
+    var posts = [Post]()
     @IBOutlet weak var profileImageView: UIImageView!
-    
+
     @IBOutlet weak var gardenImageView: UIImageView!
     
 
@@ -24,17 +26,34 @@ class FeedTableViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewWillAppear(_ animated: Bool) {
+        posts.removeAll()
+        getPosts()
+        self.tableView.reloadData()
+        tableView.estimatedRowHeight = 200
+        tableView.rowHeight = UITableViewAutomaticDimension
+        dump("posts >>>> \(self.posts)")
     }
 
+    //MARK: - Fetch data from FB
+    func getPosts() {
+        self.databaseRef.observeSingleEvent(of: .value, with: { (snapshot) in
+            for child in snapshot.children {
+                if let snap = child as? FIRDataSnapshot,
+                    let valueDict = snap.value as? [String: String] {
+                    let post = Post(userId: valueDict["userId"] ?? "", comment: valueDict["comment"] ?? "", key: snap.key )
+                    self.posts.append(post)
+                }
+            }
+            self.tableView.reloadData()
+        })
+    }
+    
     // MARK: - Table view data source
-
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return posts.count
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -42,59 +61,30 @@ class FeedTableViewController: UITableViewController {
         return 0
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
 
+        let post = posts[indexPath.row]
         // Configure the cell...
+        //cell.commentLabel.text = post.comment
+        
+        let storage = FIRStorage.storage()
+        let storageRef = storage.reference()
+        let spaceRef = storageRef.child("images/\(post.key)")
+        spaceRef.data(withMaxSize: 1 * 1024 * 1024) { data, error in
+            if let error = error {
+                print(error)
+            } else {
+                let image = UIImage(data: data!)
+//                self.gardenImageView.image = image
+            }
+        }
 
         return cell
     }
-    */
+ 
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
+ 
 
 }
